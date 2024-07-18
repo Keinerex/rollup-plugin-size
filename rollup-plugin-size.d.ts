4ca729eeb101817e7832275ca3adca3a5922ba6d
Юрков Владислав Юрьевич <urkov.v@skbkontur.ru>
2024-07-18 10:39:19 +0700

    all types is optional

--- rollup-plugin-size.d.ts
+++ rollup-plugin-size.d.ts
@@ -1,10 +1,10 @@
 declare const size: (options: {
-  compression: "none" | "gzip" | "brotli";
-  pattern: string;
-  exclude: string;
-  filename: string;
-  writeFile: boolean;
-  stripHash: (file: string) => string
+  compression?: "none" | "gzip" | "brotli";
+  pattern?: string;
+  exclude?: string;
+  filename?: string;
+  writeFile?: boolean;
+  stripHash?: (file: string) => string
 }) => {name: string; generateBundle: ({}) => void;};
 
 export default size;
